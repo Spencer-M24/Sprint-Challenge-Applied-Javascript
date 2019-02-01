6b8929580119cce1ef8cfb35cf46e85526ad2380
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-01 09:08:40 -0800

    Assign card elements

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -75,7 +75,8 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
+    
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
